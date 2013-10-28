b9a99b2b02d3d4b7a35327d1776957bf681292f1
Skylar Kelty <skylarkelty@gmail.com>
2013-10-28 21:40:31 +0000

    Add git to packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password rootpass'
 debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password rootpass'
 apt-get update
-apt-get install -y apache2 php5 php5-mcrypt php5-mysql mysql-server php5-memcache memcached
+apt-get install -y apache2 php5 php5-mcrypt php5-mysql mysql-server php5-memcache memcached git
 
 # Apache Config
 a2dissite default
