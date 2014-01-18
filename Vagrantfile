509e11594c515d6b291028906e45460f945fed84
Skylar Kelty <skylarkelty@gmail.com>
2014-01-18 13:58:02 +0000

    Mount in /mnt/remote

--- Vagrantfile
+++ Vagrantfile
@@ -4,11 +4,11 @@
 Vagrant.configure("2") do |config|
 
   config.vm.box = "precise64"
-  config.vm.box_url = "http://dl.dropbox.com/u/1537815/precise64.box"
+  config.vm.box_url = "http://puppet-vagrant-boxes.puppetlabs.com/ubuntu-server-12042-x64-vbox4210.box"
 
   config.vm.network :forwarded_port, guest: 80, host: 8080
 
-  config.vm.synced_folder "./", "/vagrant"
+  config.vm.synced_folder "../", "/mnt/remote"
 
   config.vm.provider :virtualbox do |vb|
     vb.gui = false
--- manifests/default.pp
+++ manifests/default.pp
@@ -2,17 +2,17 @@
 node 'default' {
 
 	package {
-		['memcached', 'mysql-server-5.5', 'git', 'curl']:
+		['memcached', 'mysql-server-5.5', 'git', 'curl', 'sudo', 'apache2']:
 			ensure  => 'latest',
 			require => Exec['apt_update'];
-		['apache2', 'php5', 'libapache2-mod-php5', 'php-pear', 'php5-cli', 'php5-common', 'php5-curl', 'php5-dev', 'php5-gd', 'php5-geoip', 'php5-imap', 'php5-intl', 'php5-mcrypt', 'php5-memcache', 'php5-memcached', 'php5-mysql']:
+		['php5', 'libapache2-mod-php5', 'php-pear', 'php5-cli', 'php5-common', 'php5-curl', 'php5-dev', 'php5-gd', 'php5-geoip', 'php5-imap', 'php5-intl', 'php5-mcrypt', 'php5-memcache', 'php5-memcached', 'php5-mysql']:
 			ensure  => 'latest',
 			require => Exec['apt_update'],
 			notify  => Service['apache2'];
 	}
 
 	service {
-		['apache2', 'memcached', 'mysql']:
+		['apache2', 'memcached', 'mysql', 'ssh']:
 			ensure    => true,
 			enable    => true;
 	}
@@ -24,7 +24,7 @@ node 'default' {
 			notify  => Service['apache2'];
 		'www':
 			path    => '/etc/apache2/sites-enabled/vagrant',
-			target  => '/vagrant/config/apache2.conf',
+			source  => '/mnt/remote/config/apache2.conf',
 			ensure  => link,
 			notify  => Service['apache2'];
 		'composer':
