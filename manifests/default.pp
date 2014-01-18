28ad7e4d8b7780dce544a7dde682b371e08d79a9
Skylar Kelty <skylarkelty@gmail.com>
2014-01-18 14:45:17 +0000

    Refactor: move things over to webserver module

--- manifests/default.pp
+++ manifests/default.pp
@@ -5,23 +5,15 @@ node 'base-dev' {
 		'::mysql::server':
 			root_password    => 'AnActualPassword';
 		'phpmyadmin': ;
+		'webserver': ;
 	}
 
 	package {
-		['memcached', 'git', 'curl', 'sudo', 'apache2']:
-			ensure  => 'latest',
-			require => Exec['apt_update'];
-		['php5', 'libapache2-mod-php5', 'php-pear', 'php5-cli', 'php5-common', 'php5-curl', 'php5-dev', 'php5-gd', 'php5-geoip', 'php5-imap', 'php5-intl', 'php5-mcrypt', 'php5-memcache', 'php5-memcached', 'php5-mysql']:
-			ensure  => 'latest',
-			require => Exec['apt_update'],
-			notify  => Service['apache2'];
+		['memcached', 'git', 'curl', 'sudo']:
+			ensure  => 'latest';
 	}
 
 	service {
-		'apache2':
-			ensure    => true,
-			enable    => true,
-			require   => Package['apache2'];
 		'memcached':
 			ensure    => true,
 			enable    => true,
@@ -32,15 +24,6 @@ node 'base-dev' {
 	}
 
 	file {
-		'no-default':
-			path    => '/etc/apache2/sites-enabled/000-default',
-			ensure  => absent,
-			notify  => Service['apache2'];
-		'www':
-			path    => '/etc/apache2/sites-enabled/vagrant',
-			source  => '/mnt/remote/config/apache2.conf',
-			ensure  => link,
-			notify  => Service['apache2'];
 		'composer':
 			path    => '/usr/bin/composer',
 			ensure  => present,
@@ -54,8 +37,6 @@ node 'base-dev' {
 			cwd         => '/tmp/',
 			require     => Package['curl'],
 			creates     => '/tmp/composer.phar';
-		'apt_update':
-			command     => '/usr/bin/apt-get update';
 	}
 
 }
---
+++ modules/webserver/manifests/init.pp
@@ -0,0 +1,26 @@
+class webserver {
+
+	package {
+		['apache2', 'php5', 'libapache2-mod-php5', 'php-pear', 'php5-cli', 'php5-common', 'php5-curl', 'php5-dev', 'php5-gd', 'php5-geoip', 'php5-imap', 'php5-intl', 'php5-mcrypt', 'php5-memcache', 'php5-memcached', 'php5-mysql']:
+			ensure  => 'latest';
+	}
+
+	service {
+		'apache2':
+			ensure    => true,
+			enable    => true,
+			require   => Package['apache2'];
+	}
+
+	file {
+		'no-default':
+			path    => '/etc/apache2/sites-enabled/000-default',
+			ensure  => absent,
+			notify  => Service['apache2'];
+		'www':
+			path    => '/etc/apache2/sites-enabled/vagrant',
+         	source  => 'puppet:///modules/webserver/apache2.conf',
+			ensure  => link,
+			notify  => Service['apache2'];
+	}
+}
